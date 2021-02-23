d08066a14b9408711ce867a3baf1b7e7378bcebc
chandu078 <chandudyavanapelli03@gmail.com>
2021-02-23 07:42:22 +0000

    fajita: fod: Relocate our fod HALs to vendor * Also Add vintf fragments for fod Lineage HALs.
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- device.mk
+++ device.mk
@@ -39,7 +39,7 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    lineage.biometrics.fingerprint.inscreen@1.0-service.oneplus_fajita \
+    vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.oneplus_fajita \
     vendor.oneplus.fingerprint.extension@1.0 \
     vendor.oneplus.hardware.display@1.0
 
